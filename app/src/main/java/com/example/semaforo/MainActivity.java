8bd92deb64996dae66bc6f5c6bf54be7b4fa0ae4
Lenovo <luiyimateoencinas@gmail.com>
2025-08-27 19:58:04 -0400

    fase_2

--- app/src/main/java/com/example/semaforo/MainActivity.java
+++ app/src/main/java/com/example/semaforo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.semaforo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,64 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView semaforo;
+    private int contador = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        semaforo = findViewById(R.id.semaforo);
+
+        Thread hiloSemaforo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        if (contador == 0) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    semaforo.setImageResource(R.drawable.circle_green);
+                                }
+                            });
+                            contador = 1;
+                            Thread.sleep(5000);
+                        } else if (contador == 1) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    semaforo.setImageResource(R.drawable.circle_yellow);
+                                }
+                            });
+                            contador = 2;
+                            Thread.sleep(5000);
+                        } else if (contador == 2) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    semaforo.setImageResource(R.drawable.circle_red);
+                                }
+                            });
+                            contador = 0;
+                            Thread.sleep(5000);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        hiloSemaforo.start();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
 }
